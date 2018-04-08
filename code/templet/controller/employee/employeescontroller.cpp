22028b32074aeeb0fb3d2561fe589111550df4e0
xman199775 <xman199775@gmail.com>
2018-04-08 17:27:11 +0200

    getAttr

--- code/templet/controller/employee/employeescontroller.cpp
+++ code/templet/controller/employee/employeescontroller.cpp
@@ -1,2 +1,19 @@
 #include "employeesController.h"
 
+QMap <QString,QVariant> employeesController:: getAttributeNotDiffualt(employeesModel employee)
+{
+    QMap<QString,QVariant> Attribute;
+    if(employee.getEmpId()!="")
+    {
+        Attribute.insert("Empid",employee.getEmpId());
+    }
+    if(employee.getEmployeeName()!="")
+    {
+        Attribute.insert("Name",employee.getEmployeeName());
+    }
+    if(employee.getDateBirth()!=QDate(0,0,0))
+    {
+        Attribute.insert("BirthDate",employee.getDateBirth());
+    }
+    return Attribute;
+}
--- code/templet/controller/employee/employeescontroller.h
+++ code/templet/controller/employee/employeescontroller.h
@@ -1,10 +1,13 @@
 #ifndef EMPLOYEESCONTROLLER_H
 #define EMPLOYEESCONTROLLER_H
-
-
+#include <QMap>
+#include <QVariant>
+#include<model/employee/employeesModel.h>
 class employeesController
 {
     public:
+    QMap <QString,QVariant> getAttributeNotDiffualt(employeesModel employee);
+
 
 };
 
