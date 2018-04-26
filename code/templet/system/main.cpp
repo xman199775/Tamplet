f9b4604fda89b810f5a3ccdbb844bae57198f0ef
MohamedAhmedNouh <mohamednouh@stud.fci-cu.edu.eg>
2018-04-26 13:08:21 +0200

    ignore
    ignore

--- code/templet/system/main.cpp
+++ code/templet/system/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
-    serverConnections::getInstance()->creatServerConnection("general","QMYSQL","127.0.0.1","root","root","template");
+    //serverConnections::getInstance()->creatServerConnection("general","QMYSQL","127.0.0.1","root","root","template");
     serverConnections::getInstance()->creatServerConnection("general","QMYSQL","127.0.0.1","root","root","tamplete", 8889);
     serverConnections::getInstance()->connectToServer("general");
     /*QQmlApplicationEngine engine;
