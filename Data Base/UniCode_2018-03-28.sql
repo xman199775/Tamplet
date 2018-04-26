dcdccf0a848aa1e4ef6f743034246175b91e1d63
MohamedAhmedNouh <mohamednouh@stud.fci-cu.edu.eg>
2018-04-26 13:07:48 +0200

    Efficiency Table Added
    CREATE TABLE `Efficiency` (
      `EmpId` varchar(20) NOT NULL DEFAULT '',
      `AdminId` varchar(20) NOT NULL DEFAULT '',
      `Amount` double NOT NULL DEFAULT '0',
      `Date` datetime NOT NULL,
      PRIMARY KEY (`EmpId`),
      KEY `AdminId` (`AdminId`),
      CONSTRAINT `efficiency_ibfk_1` FOREIGN KEY (`EmpId`) REFERENCES
    `Employee` (`Empid`) ON DELETE CASCADE ON UPDATE CASCADE,
      CONSTRAINT `efficiency_ibfk_2` FOREIGN KEY (`AdminId`) REFERENCES
    `Employee` (`Empid`) ON DELETE CASCADE ON UPDATE CASCADE
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

--- Data Base/UniCode_2018-03-28.sql
+++ Data Base/UniCode_2018-03-28.sql
@@ -357,6 +357,19 @@ CREATE TABLE `WareProduct` (
 
 
 
+CREATE TABLE `Efficiency` (
+  `EmpId` varchar(20) NOT NULL DEFAULT '',
+  `AdminId` varchar(20) NOT NULL DEFAULT '',
+  `Amount` double NOT NULL DEFAULT '0',
+  `Date` datetime NOT NULL,
+  PRIMARY KEY (`EmpId`),
+  KEY `AdminId` (`AdminId`),
+  CONSTRAINT `efficiency_ibfk_1` FOREIGN KEY (`EmpId`) REFERENCES `Employee` (`Empid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `efficiency_ibfk_2` FOREIGN KEY (`AdminId`) REFERENCES `Employee` (`Empid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
